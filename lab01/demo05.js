36b6ad24eacbd9dc7d5d951270892b321700d492
Nguyen Van Minh <minh.nguyen@katalon.com>
2022-01-05 23:43:21 +0700

    feature(lab01): branch command

---
+++ lab01/demo05.js
@@ -0,0 +1,30 @@
+// Câu lệnh rẽ nhánh
+
+let num = 100;
+console.log("num =", num);
+console.log("num = num + 1 =>", num + 1);
+console.log("num = num - 1 =>", num - 1);
+console.log("num = num * 2 =>", num * 2);
+console.log("num = num / 2 =>", num / 2);
+console.log("=================================");
+console.log("num = num / 3 =>", num / 3);
+console.log("num = num % 3 =>", num % 3);
+console.log("num = num / 3 =>", parseInt(num / 3));
+console.log("--------------------------------");
+console.log("num =", num);
+console.log("num++ =>", num++);
+console.log("num =", num);
+console.log("++num =>", ++num);
+console.log("num-- =>", num--);
+console.log("num =", num);
+console.log("--num =>", --num);
+console.log("********************************");
+num = 100;
+num += 5;
+console.log("num += 5 =>", num);
+num -= 4;
+console.log("num -= 4 =>", num);
+num *= 2;
+console.log("num *= 2 =>", num);
+num /= 6;
+console.log("num /= 6 =>", num);
