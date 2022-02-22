a2f576285696cb26e7f3b6feddca3a0ce69824af
Minh Nguyen Van <minh.nguyen@katalon.com>
2022-02-22 18:01:46 +0700

    first commit

---
+++ lab01/demo07.js
@@ -0,0 +1,16 @@
+/**
+ * Vòng lặp trong ES
+ */
+console.log("--- FOR loop ---");
+for (let i = 1; i <= 5; i++)
+{
+    console.log(i);
+}
+
+console.log("--- WHILE loop ---");
+let i = 0;
+while (i < 10)
+{
+    console.log(i);
+    i += 2; // i = i + 2;
+}
